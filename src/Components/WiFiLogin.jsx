2e0223a1fa34bc054c8715e8b4465e8fcc2a2b65
Rahul-9891 <133263134+Rahul-9891@users.noreply.github.com>
2024-06-25 15:16:56 +0530

    Update WiFiLogin.jsx

--- src/Components/WiFiLogin.jsx
+++ src/Components/WiFiLogin.jsx
@@ -1,30 +1,30 @@
 import React, { useState } from 'react'
 import '../styles/WiFiLogin.css'
-// import { useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 
 const WiFiLogin = () => {
 
-    // const [searchParms, setsearchParms] = useSearchParams();
+    const [searchParms, setsearchParms] = useSearchParams();
     const [name, setName] = useState({
         lastName: '',
         roomNumber: ''
     });
 
-    // const wlanId = searchParms.get('wlan_id')
-    // const client_mac = searchParms.get('client_mac')
-    // const ap_mac  = searchParms.get('ap_mac')
-    // const authorize_url = searchParms.get('authorize_url')
-    // const ap_name = searchParms.get('ap_name')
-    // const site_name = searchParms.get('site_name')
+    const wlanId = searchParms.get('wlan_id')
+    const client_mac = searchParms.get('client_mac')
+    const ap_mac  = searchParms.get('ap_mac')
+    const authorize_url = searchParms.get('authorize_url')
+    const ap_name = searchParms.get('ap_name')
+    const site_name = searchParms.get('site_name')
 
-    //  console.log(wlanId)
-    // console.log(client_mac)
-    // console.log(ap_mac)
-    // console.log(authorize_url)
-    // console.log(ap_name)
-    // console.log(site_name)
+     console.log(wlanId)
+    console.log(client_mac)
+    console.log(ap_mac)
+    console.log(authorize_url)
+    console.log(ap_name)
+    console.log(site_name)
 
-    // console.log(name)
+    console.log(name)
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -62,11 +62,11 @@ const WiFiLogin = () => {
                         required
                     />
                 </div>
-{/*                 <h1>wlanId {wlanId} </h1>
+                <h1>wlanId {wlanId} </h1>
                 <h1>client_mac {client_mac} </h1>
                 <h1>ap_mac {ap_mac} </h1>
                 <h1>authorize_url {authorize_url} </h1>
-                <h1>ap_name {ap_name} </h1> */}
+                <h1>ap_name {ap_name} </h1>
                 <p className='instructions'>Please use your last name with your room number as a username for Login</p>
                 <button type="submit" className="login-button">Login</button>
             </form>
